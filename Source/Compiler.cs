2b2c3f62a7885759815b8a42b5d42e25b72b3549
Tauram <tauramgames@gmail.com>
2026-03-30 21:44:11 +0300

    add TrailingChar option to TokenRangeString

--- Source/Compiler.cs
+++ Source/Compiler.cs
@@ -239,8 +239,8 @@ namespace Surplus
         }
 
         // GET TOKENRANGE AS STRING
-        static string TokenRangeString(List<string> Tokens, int LineIndex, string Seperator, int Start = 0, int Count = -1, string StartKeyword = "", string EndKeyword = ""){
-            return string.Join(Seperator, TokenRange(Tokens, LineIndex, Start, Count, StartKeyword, EndKeyword).ToArray());
+        static string TokenRangeString(List<string> Tokens, int LineIndex, string Seperator, int Start = 0, int Count = -1, string StartKeyword = "", string EndKeyword = "", bool TrailingChar = false){
+            return string.Join(Seperator, TokenRange(Tokens, LineIndex, Start, Count, StartKeyword, EndKeyword).ToArray()) + (TrailingChar? Seperator : "");
         }
 
         // ERROR LOGGER
